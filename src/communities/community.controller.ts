3b8f8b3e3ad4f6b77320015326ea0f934c43e04b
mmingyeomm <aliveck0208@gmail.com>
2025-04-05 02:45:13 +0900

    create_community

--- src/communities/community.controller.ts
+++ src/communities/community.controller.ts
@@ -6,11 +6,15 @@ import { AuthGuard } from '../auth/auth.guard';
 import { WalletGuard } from '../auth/wallet.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserService } from '../user/user.service';
 
 @ApiTags('communities')
 @Controller('communities')
 export class CommunityController {
-  constructor(private readonly communityService: CommunityService) {}
+  constructor(
+    private readonly communityService: CommunityService,
+    private readonly userService: UserService
+  ) {}
 
   @ApiOperation({ summary: 'Get all communities' })
   @ApiResponse({ 
@@ -48,9 +52,18 @@ export class CommunityController {
     @CurrentUser() user: any,
   ): Promise<Community> {
     console.log('User from auth:', user);
-    // Use user.xId since we're linking wallets to Twitter IDs
-    const userId = user.xId || user.id;
-    return this.communityService.createCommunity(createCommunityDto, userId);
+    
+    // Get Twitter ID from user session
+    const twitterId = user.xId || user.id;
+    
+    // Find user by Twitter ID to get their internal UUID
+    const userEntity = await this.userService.findByXId(twitterId);
+    if (!userEntity) {
+      throw new Error(`User with Twitter ID ${twitterId} not found`);
+    }
+    
+    // Use the user's internal UUID as creatorId
+    return this.communityService.createCommunity(createCommunityDto, userEntity.id);
   }
 
   @ApiOperation({ summary: 'Get community with all messages' })
--- src/communities/community.module.ts
+++ src/communities/community.module.ts
@@ -5,9 +5,13 @@ import { CommunityService } from './community.service';
 import { CommunityRepository } from './community.repository';
 import { CommunityGateway } from './community.gateway';
 import { Community } from './entities/community.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Community])],
+  imports: [
+    TypeOrmModule.forFeature([Community]),
+    UserModule
+  ],
   controllers: [CommunityController],
   providers: [CommunityService, CommunityRepository, CommunityGateway],
   exports: [CommunityService, CommunityGateway]
